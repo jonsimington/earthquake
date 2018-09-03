5c730fd098fc75bd619a0fe8dcdeb04207ac836d
Jon Simington <jon@simington.io>
2018-09-03 03:43:24 -0500

    Use new repo link for replacement settings files

--- uncomment_zinnia.sh
+++ uncomment_zinnia.sh
@@ -2,11 +2,11 @@ cd clockwork
 
 # comment urls
 mv urls.py urls.py.old
-curl -O https://raw.githubusercontent.com/jonsimington/clockwork.pw/master/clockwork/urls.py
+curl -O https://raw.githubusercontent.com/jonsimington/earthquake/master/clockwork/urls.py
 
 # comment settings
 mv settings.py settings.py.old
-curl -O https://raw.githubusercontent.com/jonsimington/clockwork.pw/master/clockwork/settings.py
+curl -O https://raw.githubusercontent.com/jonsimington/earthquake/master/clockwork/settings.py
 
 rm urls.py.old
 rm settings.py.old
